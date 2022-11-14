2eeba5cecda415bec50f61e8e5f4640ed69a2e4a
florian <florian.batt@hotmail.fr>
2022-11-14 09:51:33 +0100

    :bug:Email to bug and security fixed

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -60,23 +60,33 @@ class MessageController extends AbstractController
     public function new(Request $request, DemandRelationRepository $demandRelationRepository, DemandRepository $demandRepository, MessageRepository $messageRepository, Security $security, MailerInterface $mailer): Response
     {
             
-            $user = $security->getUser();
-            $otherUser = $request->request->get('otherUser');
-           
-            $demandId = $request->request->get('demandId');
+            $user = $security->getUser();            
             $text = $request->request->get('text');
-            $demandRelation = $demandRelationRepository->findOneBy(['demand' => $demandId, 'user' => [$user, $otherUser] ]);
-           
-           
-            $demand = $demandRepository->findOneBy(['id' => $demandId,]);
-            $demandEmail = $demand->getUser()->getEmail();
+
+            $demandRelationId = $request->request->get('DemandRelationId');
+            $demandRelation = $demandRelationRepository->findOneBy(['id' => $demandRelationId,]);
+        
+            $demandRelationUser = $demandRelation->getUser();
+            $demanduser = $demandRelation->getDemand()->getUser();
+
+            if ($user == $demanduser) {
+                $emailTo = $demandRelationUser->getEmail();
+            }
+            elseif( $user ==  $demandRelationUser){
+                $emailTo = $demanduser->getEmail();
+            }
+            else{
+                $this->addFlash('danger', 'Vous ne pouvez pas envoyer de messages sur cette conversation');
+                return $this->redirectToRoute('app_demand_index'); 
+            }
+            
             if($request->request->get("email"))
             {    
                 // dd("email");
 
                 $email = (new Email())
                 ->from('hello@example.com')      // For security reason, put an exemple on purpose to not put the actual email on gitHub. It works fine like this.
-                ->to($demandEmail)
+                ->to($emailTo)
                 //->cc('cc@example.com')
                 //->bcc('bcc@example.com')
                 //->replyTo('fabien@example.com')
@@ -101,21 +111,6 @@ class MessageController extends AbstractController
            
             
         return $this->redirectToRoute('app_my_message', ["id_demand_relation" => $demandRelation->getId()], Response::HTTP_SEE_OTHER); 
-        
-        // $message = new Message();
-        // $form = $this->createForm(MessageType::class, $message);
-        // $form->handleRequest($request);
-
-        // if ($form->isSubmitted() && $form->isValid()) {
-        //     $messageRepository->save($message, true);
-
-        //     return $this->redirectToRoute('app_message_index', [], Response::HTTP_SEE_OTHER);
-        // }
-
-        // return $this->renderForm('message/new.html.twig', [
-        //     'message' => $message,
-        //     'form' => $form,
-        // ]);
     }
 
     #[Route('/{id}', name: 'app_message_show', methods: ['GET'])]
